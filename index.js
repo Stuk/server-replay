07d2d56c2711c687d558223e01faf844fdd1029b
Stuart Knightley <stuart.knightley@adobe.com>
2014-10-17 16:29:49 -0700

    Start coding some random rules
    This will really need a cleanup

--- index.js
+++ index.js
@@ -10,25 +10,53 @@ var entries = har.log.entries;
 
 var server = http.createServer(function (request, response) {
     var entry;
-    var url = URL.parse(request.url);
+    var url = URL.parse(request.url, true);
     for (var i = 0; i < entries.length; i++) {
         entry = entries[i];
-        var entryUrl = URL.parse(entry.request.url);
+        var entryUrl = URL.parse(entry.request.url, true);
         if (
             request.method === entry.request.method &&
             url.pathname === entryUrl.pathname
         ) {
-            console.log("Found entry", entry.request.url, "for", request.url);
+            if (/^\/api/.test(url.pathname)) {
+                if (
+                    url.query.q === entryUrl.query.q &&
+                    url.query["X-Location"] === entryUrl.query["X-Location"]
+                ) {
+                    if (request.headers["x-location"]) {
+                        if (entry.request.headers.some(function (header) {
+                            return header.name.toLowerCase() === "x-location" && header.value === request.headers["x-location"];
+                        })) {
+                            break;
+                        } else {
+                            continue;
+                        }
+                    }
+                    console.log(request.url, "->", entry.request.url);
+                    break;
+                } else {
+                    continue;
+                }
+            }
             break;
         }
     }
 
     if (i === entries.length) {
-        console.log("Not found", request.url);
+        console.log("Not found:", request.url);
+        // console.log(request.headers);
         response.writeHead(404, "Not found", {"content-type": "text/plain"});
         response.end("404 Not found");
         return;
     }
+
+    // var cookieNames = request.headers.cookie.split(";").map(function (c) { return c.split("=")[0]; });
+    // entry.request.cookies.forEach(function (cookie) {
+    //     if (cookieNames.indexOf(cookie.name) === -1) {
+    //         // console.log("Missing cookie, in the console run:");
+    //         console.log("document.cookie = " + JSON.stringify(cookie.name + "=" + cookie.value));
+    //     }
+    // });
 
     for (var h = 0; h < entry.response.headers.length; h++) {
         var name = entry.response.headers[h].name;
@@ -50,7 +78,9 @@ var server = http.createServer(function (request, response) {
     }
 
     response.statusCode = entry.response.status;
-    response.end(entry.response.content.text);
+    var content = entry.response.content.text.replace("scproxy-stage.adobecc.com", "localhost:" + PORT);
+    content = content.replace("https://localhost", "http://localhost");
+    response.end(content);
 });
 
 server.listen(PORT);
